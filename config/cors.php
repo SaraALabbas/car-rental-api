f17a5b52669edfa5dbf9693700e1ab3f6bad00c3
Sara ALabbas <saraalabbas1910@gmail.com>
2026-08-08 20:50:31 +0300

    fix cors for vercel frontend

--- config/cors.php
+++ config/cors.php
@@ -25,11 +25,8 @@ return [
 'allowed_origins' => [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
-],
-//  'allowed_origins' => [
-//         'https://car-rentalv1.vercel.app',
-//     ],
-    'allowed_origins_patterns' => [],
+    'https://car-rental-lyart-gamma.vercel.app',
+],    'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
